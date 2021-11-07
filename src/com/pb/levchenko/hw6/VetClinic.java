a762247d7d740114f6e5ef45c265f98e9a164ff8
Aleksandra Levchenko <aleksandra.privatbank.ua>
2021-11-07 11:00:36 +0200

    init commit

--- src/com/pb/levchenko/hw6/VetClinic.java
+++ src/com/pb/levchenko/hw6/VetClinic.java
@@ -2,12 +2,11 @@ package com.pb.levchenko.hw6;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
-
 public class VetClinic<object> extends Veterinarian {
     public VetClinic() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Конструктор
         Animal dog = new Animal(" Комиссар Рекс ", " корм ", " полицейский участок");
         Animal cat = new Animal(" Салем ", " сухой корм", " планета Кошек ");
@@ -21,25 +20,36 @@ public class VetClinic<object> extends Veterinarian {
         }
         // Демонстарациия метода который печатает на экран food и location
         // пришедшего на прием животного
-        treatAnimal(dog);
-        treatAnimal(cat);
-        treatAnimal(horse);
+      // Veterinarian.treatAnimal(dog);
+      //  Veterinarian.treatAnimal(cat);
+      //  Veterinarian.treatAnimal(horse);
 
         // В цикле животные отправляются на прием к ветеринару.
         Random random = new Random();
         Animal animal = animals[random.nextInt(3)];
         if (animal.equals(dog)) {
             System.out.println(" Пес идет на прием к ветеренару ");
+            treatAnimal(dog);
         } else if (animal.equals(cat)) {
             System.out.println(" Кот идет на прием к ветеренару ");
+            treatAnimal(cat);
         } else if (animal.equals(horse)) {
             System.out.println("Лошадка идет на прием к ветеренару ");
-        }
-        else {
+            treatAnimal(horse);
+        } else {
             System.out.println();
         }
+    Veterinarian v = new Veterinarian();
+    Class clazz = v.getClass();
+    Class vetClazz = Class.forName("com.pb.levchenko.hw6.Veterinarian");
+    Object obj = vetClazz.newInstance();
+    if (obj instanceof Veterinarian) {
+        // Тест объекта
+        System.out.println();
+        ((Veterinarian)obj).treatAnimal(cat);
+    }
 
-     }
+    }
 }
 
 
--- src/com/pb/levchenko/hw6/Veterinarian.java
+++ src/com/pb/levchenko/hw6/Veterinarian.java
@@ -1,8 +1,8 @@
 package com.pb.levchenko.hw6;
 
-import java.lang.reflect.Constructor;
-
 public class Veterinarian {
+
+
 
 
     public static void treatAnimal(Animal animal) {
@@ -10,23 +10,8 @@ public class Veterinarian {
                 " " + "место обитания: " + animal.getLocation());
     }
 
-    public static class Main {
-        public static void main(String[] args) throws Exception {
-
-            Veterinarian v = new Veterinarian();
-            Class clazz = v.getClass();
-            System.out.println(clazz.getName());
-            Class vetClazz = Class.forName("com.pb.levchenko.hw6.Veterinarian");
-
-            Constructor constructor = vetClazz.getConstructor(new Class[]{String.class});
-            Object obj = constructor.newInstance("Ветеринар Айболит");
-            if (obj instanceof Veterinarian) {
-                ((Veterinarian) obj).treatAnimal();
             }
 
-        }
-    }
-}
 
 
 
